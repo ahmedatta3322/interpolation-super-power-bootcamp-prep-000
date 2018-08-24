9e146329be119d38d64fb64f55741b25d380fc81
ahmed atta <ahmedatta3322@gmail.com>
2018-08-24 11:47:57 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,4 +1,4 @@
 # Write your #display_rainbow method here
 def display_rainbow(argx)
-  puts "R: #{ argx[0] }, O: orange, Y: yellow, G: green, B: blue, I: indigo, V: violet"
+  puts "R: #{ argx[0] }, O: #{ argx[1] }, Y: #{ argx[2] }, G: #{ argx[3] }, B: #{ argx[4] }, I: #{ argx[5] }, V: #{ argx[6] }"
 end
